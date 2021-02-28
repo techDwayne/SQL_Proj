5bf2a7f095972fddc26a4a2be4e5ab6da2780e11
techD <ldsudduth@live.com>
2021-02-28 13:02:05 -0500

    Update Sql_Scripts.sql

--- Sql_Scripts.sql
+++ Sql_Scripts.sql
@@ -62,4 +62,45 @@ begin
 		end
 end
 
+--Over(), PARTITION BY, and ORDER BY clause
+
+--Over() 
+select A.EmployeeNumber, year(A.AttendanceMonth) as AttendanceYear, 
+sum(A.NumberAttendance) over() as TotalAttendance,
+convert(decimal (18,7),A.NumberAttendance) /sum(A.NumberAttendance) over() * 100 as PercentageAttendance --shows percentage of annual attendance per month over a given year
+from tblEmployee as E join tblAttendance as A
+on e.EmployeeNumber = A.EmployeeNumber
+--group by A.EmployeeNumber, year(A.AttendanceMonth) 
+--order by A.EmployeeNumber, year(A.AttendanceMonth) 
+where A.AttendanceMonth < '20150101'
+
+--Over(partition by)
+select A.EmployeeNumber, year(A.AttendanceMonth) as AttendanceYear, 
+sum(A.NumberAttendance) over(partition by A.EmployeeNumber) as TotalAttendance,
+convert(decimal (18,7),A.NumberAttendance) /sum(A.NumberAttendance) over(partition by A.EmployeeNumber) * 100 as PercentageAttendance --shows percentage of attendance for a given year, per Employee
+from tblEmployee as E join tblAttendance as A
+on e.EmployeeNumber = A.EmployeeNumber
+--group by A.EmployeeNumber, year(A.AttendanceMonth) 
+--order by A.EmployeeNumber, year(A.AttendanceMonth) 
+where A.AttendanceMonth < '20150101'
+
+--over(partition by order by)
+select A.EmployeeNumber, year(A.AttendanceMonth) as AttendanceYear, 
+sum(A.NumberAttendance) over(partition by A.EmployeeNumber order by A.AttendanceMonth) as RunningTotal --keeps running total for the attendance year
+--convert(decimal (18,7),A.NumberAttendance) /sum(A.NumberAttendance) over(partition by A.EmployeeNumber order by A.AttendanceMonth) * 100 as PercentageAttendance
+from tblEmployee as E join tblAttendance as A
+on e.EmployeeNumber = A.EmployeeNumber
+--group by A.EmployeeNumber, year(A.AttendanceMonth) 
+--order by A.EmployeeNumber, year(A.AttendanceMonth) 
+where A.AttendanceMonth < '20150101'
+
+--over() with rows between keywords preceding and following
+select A.EmployeeNumber, year(A.AttendanceMonth) as AttendanceYear, 
+sum(A.NumberAttendance) 
+over(partition by A.EmployeeNumber 
+	order by A.AttendanceMonth
+	rows between 1 preceding and 1 following) as RunningTotal
+from tblEmployee as E join tblAttendance as A
+on e.EmployeeNumber = A.EmployeeNumber
+where A.AttendanceMonth < '20150101'
 
